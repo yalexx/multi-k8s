7395f00373c28daed0a77d10056797c9e6fbb1f7
Yanko <superactro@gmail.com>
2019-05-17 10:12:19 +0300

    fix deploy bug

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push wemaptech/multi-server:$SHA
 docker push wemaptech/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=wematech/multi-server:$SHA
-kubectl set image deployments/client-deployment client=wematech/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=wematech/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=wemaptech/multi-server:$SHA
+kubectl set image deployments/client-deployment client=wemaptech/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=wemaptech/multi-worker:$SHA
